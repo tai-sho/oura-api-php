2c51568b1f8b2d209b0ae1ae5e7e08ab9c27d4bc
ShoheiTai <s_tai@spice-factory.co.jp>
2024-06-08 14:30:54 +0900

    Update client

--- src/Client.php
+++ src/Client.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace OuraApiPhp;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Psr\Http\Message\ResponseInterface;
 
 class Client
@@ -14,20 +15,23 @@ class Client
     public function __construct(string $accessToken)
     {
         $this->httpClient = new GuzzleClient([
-            'base_uri' => 'https://api.ouraring.com/v2/'
+            'base_uri' => 'https://api.ouraring.com/v2/',
+            'headers' => [
+                'Authorization' => "Bearer {$accessToken}",
+                'Accept' => 'application/json',
+            ]
         ]);
         $this->accessToken = $accessToken;
     }
 
     private function request(string $method, string $endpoint, array $params = []): ResponseInterface
     {
-        return $this->httpClient->request($method, $endpoint, [
-            'headers' => [
-                'Authorization' => "Bearer {$this->accessToken}",
-                'Accept' => 'application/json'
-            ],
-            'query' => $params
-        ]);
+        try {
+            return $this->httpClient->request($method, $endpoint, ['query' => $params]);
+        } catch (GuzzleException $e) {
+            // HTTPリクエストが失敗した場合の例外処理
+            throw new \RuntimeException('HTTP request failed: ' . $e->getMessage(), 0, $e);
+        }
     }
 
     public function get(string $endpoint, array $params = []): ResponseInterface
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace OuraApiPhp\Tests;
@@ -7,33 +6,64 @@ namespace OuraApiPhp\Tests;
 use OuraApiPhp\Client;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
 
 class ClientTest extends TestCase
 {
+    private $mockHttpClient;
+    private $accessToken = 'your-access-token';
+
+    protected function setUp(): void
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(['key' => 'value'])),
+            new RequestException("Error Communicating with Server", new Request('GET', 'some-endpoint'))
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $this->mockHttpClient = new GuzzleClient(['handler' => $handlerStack]);
+    }
+
     public function testCanBeInstantiated(): void
     {
-        $client = new Client('your-access-token');
+        $client = new Client($this->accessToken);
         $this->assertInstanceOf(Client::class, $client);
     }
 
     public function testGetRequest(): void
     {
-        $mockResponse = new Response(200, [], json_encode(['key' => 'value']));
-        $mock = new \GuzzleHttp\Handler\MockHandler([$mockResponse]);
-        $handlerStack = \GuzzleHttp\HandlerStack::create($mock);
-        $httpClient = new \GuzzleHttp\Client(['handler' => $handlerStack]);
-
-        $client = new Client('your-access-token');
-        $clientReflection = new \ReflectionClass(Client::class);
-        $property = $clientReflection->getProperty('httpClient');
+        $client = new Client($this->accessToken);
+        $reflection = new \ReflectionClass($client);
+        $property = $reflection->getProperty('httpClient');
         $property->setAccessible(true);
-        $property->setValue($client, $httpClient);
+        $property->setValue($client, $this->mockHttpClient);
 
         $response = $client->get('some-endpoint');
         $data = json_decode($response->getBody()->getContents(), true);
 
         $this->assertSame(200, $response->getStatusCode());
         $this->assertSame(['key' => 'value'], $data);
+    }
+
+    public function testGetRequestThrowsException(): void
+    {
+        $mock = new MockHandler([
+            new RequestException("Error Communicating with Server", new Request('GET', 'some-endpoint'))
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $mockHttpClient = new GuzzleClient(['handler' => $handlerStack]);
+
+        $client = new Client($this->accessToken);
+        $reflection = new \ReflectionClass($client);
+        $property = $reflection->getProperty('httpClient');
+        $property->setAccessible(true);
+        $property->setValue($client, $mockHttpClient);
+
+        $this->expectException(\RuntimeException::class);
+        $client->get('some-endpoint');
     }
 }
 
