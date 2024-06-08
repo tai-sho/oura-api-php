fff60c8d07e15c183f56f951e9cc9d0ffa0fe139
ShoheiTai <s_tai@spice-factory.co.jp>
2024-06-08 15:12:05 +0900

    Add Endpoint

---
+++ src/OuraApiClient.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OuraApiPhp;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
+
+class OuraApiClient
+{
+    private GuzzleClient $httpClient;
+    private string $accessToken;
+    private string $baseUri = 'https://api.ouraring.com';
+    private string $apiVersion;
+
+    public function __construct(string $accessToken, string $apiVersion = 'v2')
+    {
+        $this->httpClient = new GuzzleClient([
+            'base_uri' => $this->baseUri,
+            'headers' => [
+                'Authorization' => "Bearer {$accessToken}",
+                'Accept' => 'application/json',
+            ],
+        ]);
+        $this->accessToken = $accessToken;
+        $this->apiVersion = $apiVersion;
+    }
+
+    private function makeRequest(string $method, string $endpoint, array $params = []): ResponseInterface
+    {
+        $uri = sprintf('/%s/%s', $this->apiVersion, $endpoint);
+        $options = [];
+        if (!empty($params)) {
+            $options['query'] = $params;
+        }
+
+        try {
+            return $this->httpClient->request($method, $uri, $options);
+        } catch (GuzzleException $e) {
+            throw new RuntimeException('Request failed: ' . $e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function getHeartRate(array $params = []): ResponseInterface
+    {
+        return $this->makeRequest('GET', 'usercollection/heartrate', $params);
+    }
+
+    public function getWorkout(array $params = []): ResponseInterface
+    {
+        return $this->makeRequest('GET', 'usercollection/workout', $params);
+    }
+
+    public function getAllPages(string $endpoint, array $params = []): array
+    {
+        $allResponses = [];
+        $nextToken = null;
+
+        do {
+            if ($nextToken) {
+                $params['next_token'] = $nextToken;
+            }
+
+            $response = $this->makeRequest('GET', $endpoint, $params);
+            $allResponses[] = $response;
+
+            $data = json_decode($response->getBody()->getContents(), true);
+            $nextToken = $data['next_token'] ?? null;
+
+        } while ($nextToken);
+
+        return $allResponses;
+    }
+
+    public function getAllHeartRateData(array $params = []): array
+    {
+        return $this->getAllPages('usercollection/heartrate', $params);
+    }
+
+    public function getAllWorkoutData(array $params = []): array
+    {
+        return $this->getAllPages('usercollection/workout', $params);
+    }
+
+}
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,16 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 namespace OuraApiPhp\Tests;
 
-use OuraApiPhp\Client;
+use OuraApiPhp\OuraApiClient;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\ResponseInterface;
 
 class ClientTest extends TestCase
 {
@@ -20,8 +20,8 @@ class ClientTest extends TestCase
     protected function setUp(): void
     {
         $mock = new MockHandler([
-            new Response(200, [], json_encode(['key' => 'value'])),
-            new RequestException("Error Communicating with Server", new Request('GET', 'some-endpoint'))
+            new Response(200, [], json_encode(['key' => 'value', 'next_token' => 'next123'])),
+            new Response(200, [], json_encode(['key' => 'value2'])),
         ]);
         $handlerStack = HandlerStack::create($mock);
         $this->mockHttpClient = new GuzzleClient(['handler' => $handlerStack]);
@@ -29,41 +29,39 @@ class ClientTest extends TestCase
 
     public function testCanBeInstantiated(): void
     {
-        $client = new Client($this->accessToken);
-        $this->assertInstanceOf(Client::class, $client);
+        $client = new OuraApiClient($this->accessToken);
+        $this->assertInstanceOf(OuraApiClient::class, $client);
     }
 
-    public function testGetRequest(): void
+    public function testGetHeartRate(): void
     {
-        $client = new Client($this->accessToken);
+        $client = new OuraApiClient($this->accessToken);
         $reflection = new \ReflectionClass($client);
         $property = $reflection->getProperty('httpClient');
         $property->setAccessible(true);
         $property->setValue($client, $this->mockHttpClient);
 
-        $response = $client->get('some-endpoint');
+        $response = $client->getHeartRate(['start_date' => '2023-01-01', 'end_date' => '2023-01-07']);
         $data = json_decode($response->getBody()->getContents(), true);
 
+        $this->assertInstanceOf(ResponseInterface::class, $response);
         $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame(['key' => 'value'], $data);
+        $this->assertSame(['key' => 'value', 'next_token' => 'next123'], $data);
     }
 
-    public function testGetRequestThrowsException(): void
+    public function testGetAllHeartRateData(): void
     {
-        $mock = new MockHandler([
-            new RequestException("Error Communicating with Server", new Request('GET', 'some-endpoint'))
-        ]);
-        $handlerStack = HandlerStack::create($mock);
-        $mockHttpClient = new GuzzleClient(['handler' => $handlerStack]);
-
-        $client = new Client($this->accessToken);
+        $client = new OuraApiClient($this->accessToken);
         $reflection = new \ReflectionClass($client);
         $property = $reflection->getProperty('httpClient');
         $property->setAccessible(true);
-        $property->setValue($client, $mockHttpClient);
+        $property->setValue($client, $this->mockHttpClient);
 
-        $this->expectException(\RuntimeException::class);
-        $client->get('some-endpoint');
+        $responses = $client->getAllHeartRateData(['start_date' => '2023-01-01', 'end_date' => '2023-01-07']);
+
+        $this->assertCount(2, $responses);
+        $this->assertInstanceOf(ResponseInterface::class, $responses[0]);
+        $this->assertInstanceOf(ResponseInterface::class, $responses[1]);
     }
 }
 
